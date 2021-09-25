f9bdc9b9df1721a18b161a511692849c1800259d
dante01yoon <bunggl@naver.com>
2021-09-25 20:02:56 +0900

    Update: api dedirectory 옮김

---
+++ api/card/_helpers.js
@@ -0,0 +1,26 @@
+import mysql from "mysql2";
+
+export const selectQuery = `SELECT * FROM words`
+
+
+export function initDB() {
+  const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  db.connect((err) => {
+    if (err) {
+      console.log("err on connection: ", err);
+      throw Error({
+        type: "DB_CONNECTION_ERROR",
+        message: err,
+      })
+    }
+  });
+
+  return { db };
+
+}
