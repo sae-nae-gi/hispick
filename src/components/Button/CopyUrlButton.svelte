d6e9814e580066816637a6b8d1c3d317f043e822
dante01yoon <bunggl@naver.com>
2021-09-30 20:33:17 +0900

    Update: 링크주소 복사 버튼, 토스트 생성

---
+++ src/components/Button/CopyUrlButton.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  export let link: string;
+  export let onAfterClick: () => void;
+  export let isCopied;
+  
+  const handleClickButton = () => {
+    if(navigator.clipboard){
+      navigator.clipboard.writeText(link);
+      isCopied = true;
+    }
+    else {
+      (document.getElementById("link_container") as HTMLTextAreaElement)?.select();
+      document.execCommand("copy");
+      isCopied = true;
+    }
+    onAfterClick();
+  }
+</script>
+
+<div id="sns_button_container" on:click={handleClickButton}>
+  링크 공유
+</div>
+<textarea bind:value={link} id="link_container" />
+
+<style>
+  #sns_button_container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #0acf83;
+    font-family: SangSangFlowerRoad, Noto Sans KR, Roboto;
+    font-size: 20px;
+    width: 68px;
+    height: 68px;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+  textarea {
+    width: 0px;
+    height: 0px;
+    visibility: hidden;
+  }
+</style>
